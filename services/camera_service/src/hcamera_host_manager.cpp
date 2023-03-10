7c304157aa145868660e7a70f1315d64280d46ea
zhang <zhangfengxi@huawei.com>
2023-03-10 10:55:23 +0800

    fix:Eliminate the dependency on the compilation path of bin hdf_core
    Signed-off-by: zhang <zhangfengxi@huawei.com>

--- services/camera_service/src/hcamera_host_manager.cpp
+++ services/camera_service/src/hcamera_host_manager.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,7 +18,7 @@
 #include "v1_0/icamera_host_callback.h"
 #include "metadata_utils.h"
 #include "camera_util.h"
-#include "hdf_io_service_if.h"
+#include "hdf_device_class.h"
 #include "iproxy_broker.h"
 #include "iservmgr_hdi.h"
 #include "camera_log.h"
