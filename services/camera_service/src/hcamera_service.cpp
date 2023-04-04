f9f65bb8ac55db4ab3575028b538164db0495f4a
马利 <mali81@huawei.com>
2023-04-04 03:08:16 +0000

    修复调用cameraservice.onflashlightstatus crash 跟 调用capturesession release crash
    Signed-off-by: 马利 <mali81@huawei.com>

--- services/camera_service/src/hcamera_service.cpp
+++ services/camera_service/src/hcamera_service.cpp
@@ -351,7 +351,9 @@ void HCameraService::OnCameraStatus(const std::string& cameraId, CameraStatus st
                           IPCSkeleton::GetCallingPid());
             continue;
         }
-        it.second->OnCameraStatusChanged(cameraId, status);
+        if (it.second) {
+            it.second->OnCameraStatusChanged(cameraId, status);
+        }
         CAMERA_SYSEVENT_BEHAVIOR(CreateMsg("OnCameraStatusChanged! for cameraId:%s, current Camera Status:%d",
                                            cameraId.c_str(), status));
     }
@@ -369,7 +371,9 @@ void HCameraService::OnFlashlightStatus(const std::string& cameraId, FlashStatus
                           IPCSkeleton::GetCallingPid());
             continue;
         }
-        it.second->OnFlashlightStatusChanged(cameraId, status);
+        if (it.second) {
+            it.second->OnFlashlightStatusChanged(cameraId, status);
+        }
     }
 }
 
