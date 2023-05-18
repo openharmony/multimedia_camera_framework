6451e06c99c71679b3f0e076c938280f50603426
mali <mali81@huawei.com>
2023-05-18 15:00:05 +0800

    focus and metering point range correction
    Signed-off-by: mali <mali81@huawei.com>

--- interfaces/inner_api/native/camera/include/session/capture_session.h
+++ interfaces/inner_api/native/camera/include/session/capture_session.h
@@ -636,6 +636,7 @@ private:
     int32_t UpdateSetting(std::shared_ptr<OHOS::Camera::CameraMetadata> changedMetadata);
     void SetFrameRateRange(const std::vector<int32_t>& frameRateRange);
     Point CoordinateTransform(Point point);
+    Point VerifyFocusCorrectness(Point point);
 };
 } // namespace CameraStandard
 } // namespace OHOS
