593732a970695d8ce657d125dbb8f16e8d7db500
罗长城 <luochangcheng1@h-partners.com>
2023-12-05 07:48:08 +0000

    update frameworks/native/camera/src/output/sketch_wrapper.cpp. fix code_check
    Signed-off-by: 罗长城 <luochangcheng1@h-partners.com>

--- frameworks/native/camera/src/output/sketch_wrapper.cpp
+++ frameworks/native/camera/src/output/sketch_wrapper.cpp
@@ -287,7 +287,7 @@ float SketchWrapper::GetSketchReferenceFovRatio(int32_t modeName, float zoomRati
         if (currentZoomRatio - maxRange.zoomMax >= -std::numeric_limits<float>::epsilon()) {
             return maxRange.referenceValue;
         }
-        std::vector<SketchReferenceFovRange>::iterator itRange = std::find_if(it->second.begin(), it->second.end()
+        std::vector<SketchWrapper::SketchReferenceFovRange>::iterator itRange = std::find_if(it->second.begin(), it->second.end()
             [&currentZoomRatio, std::numeric_limits<float>::epsilon()](const auto& range) {
             return currentZoomRatio - range.zoomMin >= -std::numeric_limits<float>::epsilon() &&
             currentZoomRatio - range.zoomMax < -std::numeric_limits<float>::epsilon()
