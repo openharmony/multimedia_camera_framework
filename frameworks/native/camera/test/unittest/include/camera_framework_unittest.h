0acaed13f4e4649d481dc7af1d1c45bdd9150e3b
zhoujing <zhoujing96@huawei.com>
2024-01-15 15:53:40 +0800

    fix tdd
    Signed-off-by: zhoujing <zhoujing96@huawei.com>

--- frameworks/native/camera/test/unittest/include/camera_framework_unittest.h
+++ frameworks/native/camera/test/unittest/include/camera_framework_unittest.h
@@ -36,6 +36,8 @@ public:
     static const int32_t VIDEO_DEFAULT_HEIGHT = 360;
     static const int32_t DEFAULT_MODE = 0;
     static const int32_t PORTRAIT_MODE = 3;
+    static const int32_t NIGHT_MODE = 4;
+    static const int32_t SCAN_MODE = 7;
     static const int32_t MODE_FINISH = -1;
     static const int32_t STREAM_FINISH = -1;
     static const int32_t PREVIEW_STREAM = 0;
