f17c92a1ada1523168cab1aaac3d274bb8f546dc
qano <nimaosen1@huawei.com>
2024-01-26 11:17:35 +0800

    fixed d40d8ad from https://gitee.com/qano/multimedia_camera_framework/pulls/1188 add xxxSessionForSys
    Signed-off-by: qano <nimaosen1@huawei.com>

--- frameworks/js/camera_napi/src/mode/video_session_napi.cpp
+++ frameworks/js/camera_napi/src/mode/video_session_napi.cpp
@@ -48,8 +48,7 @@ napi_value VideoSessionNapi::Init(napi_env env, napi_value exports)
     napi_status status;
     napi_value ctorObj;
     std::vector<std::vector<napi_property_descriptor>> descriptors = {camera_process_props,
-        flash_props, auto_exposure_props, focus_props, zoom_props, filter_props, beauty_props,
-        color_effect_props, macro_props, color_management_props, stabilization_props};
+        flash_props, auto_exposure_props, focus_props, zoom_props, filter_props};
     std::vector<napi_property_descriptor> video_session_props = CameraNapiUtils::GetPropertyDescriptor(descriptors);
     status = napi_define_class(env, VIDEO_SESSION_NAPI_CLASS_NAME, NAPI_AUTO_LENGTH,
                                VideoSessionNapiConstructor, nullptr,
