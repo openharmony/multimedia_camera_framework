0a25a4ec2d804bff78f68442ec3a10b09d19ec2e
wuwenchao <wuwenchao7@huawei.com>
2024-05-29 02:14:23 +0000

    Revert "!1441 Add device conflict handler"
    This reverts commit 38c36091548a8d9200d189a91be9003c1bb8e07a.
    
    Signed-off-by: wuwenchao <wuwenchao7@huawei.com>

--- services/camera_service/include/hcamera_device_manager.h
+++ services/camera_service/include/hcamera_device_manager.h
@@ -24,122 +24,6 @@
 
 namespace OHOS {
 namespace CameraStandard {
-
-
-class CameraProcessPriority : public RefBase {
-public:
-    CameraProcessPriority(int32_t uid, int32_t state, int32_t focusState) : processUid_(uid),
-        processState_(state), focusState_(focusState) {}
-
-    bool operator == (const CameraProcessPriority& rhs) const
-    {
-        return (this->processState_ == rhs.processState_) && (this->focusState_ == rhs.focusState_);
-    }
-
-    bool operator < (const CameraProcessPriority& rhs) const
-    {
-        if (this->processUid_ < maxSysUid_) {
-            MEDIA_DEBUG_LOG("this->processUid_ :%{public}d", this->processUid_);
-            return true;
-        } else if (this->processUid_ >= maxSysUid_ && rhs.processUid_ < maxSysUid_) {
-            MEDIA_DEBUG_LOG("this->processUid_ :%{public}d, rhs.processUid_ :%{public}d",
-                this->processUid_, rhs.processUid_);
-            return false;
-        }
-        if (this->processState_ == rhs.processState_) {
-            MEDIA_DEBUG_LOG("this->processState_ :%{public}d == rhs.processState_: %{public}d",
-                this->processState_, rhs.processState_);
-            return this->focusState_ < rhs.focusState_;
-        } else {
-            MEDIA_DEBUG_LOG("this->processState:%{public}d, rhs.processState_:%{public}d",
-                this->processState_, rhs.processState_);
-            return this->processState_ > rhs.processState_;
-        }
-    }
-
-    bool operator > (const CameraProcessPriority& rhs) const
-    {
-        return rhs < *this;
-    }
-
-    bool operator <= (const CameraProcessPriority& rhs) const
-    {
-        return !(*this > rhs);
-    }
-
-    bool operator >= (const CameraProcessPriority& rhs) const
-    {
-        return !(*this < rhs);
-    }
-
-    void SetProcessState(int32_t state)
-    {
-        processState_ = state;
-    }
-
-    void SetProcessFocusState(int32_t focusState)
-    {
-        focusState_ = focusState;
-    }
-
-    int32_t GetUid() const{ return processUid_; }
-
-    int32_t GetState() const { return processState_; }
-
-    int32_t GetFocusState() const { return focusState_; }
-
-private:
-    const int32_t maxSysUid_ = 10000;
-    int32_t processUid_;
-    int32_t processState_;
-    int32_t focusState_;
-};
-
-class HCameraDeviceHolder : public RefBase {
-public:
-    HCameraDeviceHolder(int32_t pid, int32_t uid, int32_t state, int32_t focusState,
-        sptr<HCameraDevice> device, uint32_t accessTokenId)
-        :pid_(pid), uid_(uid), state_(state), focusState_(focusState), accessTokenId_(accessTokenId), device_(device)
-    {
-        processPriority_ = new CameraProcessPriority(uid, state, focusState);
-    }
-    void SetPid(int32_t pid) { pid_ = pid; }
-    void SetUid(int32_t uid) { uid_ = uid; }
-    void SetState(int32_t state)
-    {
-        processPriority_->SetProcessState(state);
-        state_ = state;
-    }
-    void SetFocusState(int32_t focusState)
-    {
-        processPriority_->SetProcessFocusState(focusState);
-        focusState_ = focusState;
-    }
-
-    int32_t GetPid() const {return pid_;}
-
-    int32_t GetUid() const{ return uid_; }
-
-    int32_t GetState() const { return state_; }
-
-    int32_t GetFocusState() const { return focusState_; }
-
-    uint32_t GetAccessTokenId() const { return accessTokenId_; }
-
-    sptr<HCameraDevice> GetDevice() const { return device_; }
-    
-    sptr<CameraProcessPriority> GetPriority() const {return processPriority_;}
-
-private:
-    int32_t pid_;
-    int32_t uid_;
-    int32_t state_;
-    int32_t focusState_;
-    uint32_t accessTokenId_;
-    sptr<CameraProcessPriority> processPriority_;
-    sptr<HCameraDevice> device_;
-};
-
 class HCameraDeviceManager : public RefBase {
 public:
     ~HCameraDeviceManager();
@@ -166,18 +50,11 @@ public:
     void RemoveDevice();
 
     /**
-    * @brief Get cameraHolder by active process pid.
-    *
-    * @param pid Pid of active process.
-    */
-    sptr<HCameraDeviceHolder> GetCameraHolderByPid(pid_t pid);
-
-    /**
     * @brief Get cameras by active process pid.
     *
     * @param pid Pid of active process.
     */
-    sptr<HCameraDevice> GetCameraByPid(pid_t pidRequest);
+    sptr<HCameraDevice> GetCameraByPid(pid_t pid);
 
     /**
     * @brief Get process pid device manager instance.
@@ -203,15 +80,13 @@ private:
     HCameraDeviceManager();
     static sptr<HCameraDeviceManager> cameraDeviceManager_;
     static std::mutex instanceMutex_;
-    SafeMap<pid_t, sptr<HCameraDeviceHolder>> pidToCameras_;
+    SafeMap<pid_t, sptr<HCameraDevice>> pidToCameras_;
     SafeMap<std::string, int32_t> stateOfACamera_;
     std::mutex mapMutex_;
     sptr<ICameraBroker> PeerCallback_;
     std::string GetACameraId();
-    bool IsAllowOpen(pid_t activeClient);
-    void UpdateProcessState(int32_t& activeState, int32_t& requestState,
-        uint32_t activeAccessTokenId, uint32_t requestAccessTokenId);
-    void PrintClientInfo(sptr<HCameraDeviceHolder> activeCameraHolder, sptr<HCameraDeviceHolder> requestCameraHolder);
+    int32_t GetAdjForCameraState(std::string cameraId);
+    bool isAllowOpen(pid_t activeClient);
 };
 } // namespace CameraStandard
 } // namespace OHOS
