6c0e2b4f63fc9d0dabe102aa25842addc6063a59
zhanglei <zhanglei737@huawei.com>
2024-06-14 15:18:55 +0800

    fix updatesetting no effect due to s&r
    Signed-off-by: zhanglei <zhanglei737@huawei.com>

--- services/camera_service/src/hcamera_service.cpp
+++ services/camera_service/src/hcamera_service.cpp
@@ -1632,6 +1632,12 @@ std::shared_ptr<OHOS::Camera::CameraMetadata> HCameraService::CreateDefaultSetti
         uint8_t filterValue = item.data.u8[0];
         defaultSettings->addEntry(OHOS_CONTROL_FILTER_TYPE, &filterValue, count);
     }
+
+    ret = OHOS::Camera::FindCameraMetadataItem(currentSetting->get(), OHOS_CONTROL_EFFECT_SUGGESTION, &item);
+    if (ret == CAM_META_SUCCESS) {
+        uint8_t enableValue = item.data.u8[0];
+        defaultSettings->addEntry(OHOS_CONTROL_EFFECT_SUGGESTION, &enableValue, count);
+    }
     return defaultSettings;
 }
 
