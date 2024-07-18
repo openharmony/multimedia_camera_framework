4a89b12afe8c7162e3ac0289b96ec7f24465cb48
张廷建 <zhangtingjian@h-partners.com>
2024-07-18 13:28:44 +0000

    update frameworks/js/camera_napi/src/output/photo_output_napi.cpp.
    Signed-off-by: 张廷建 <zhangtingjian@h-partners.com>

--- frameworks/js/camera_napi/src/output/photo_output_napi.cpp
+++ frameworks/js/camera_napi/src/output/photo_output_napi.cpp
@@ -247,6 +247,10 @@ void PhotoListener::CreateMediaLibrary(sptr<SurfaceBuffer> surfaceBuffer, Buffer
         size = static_cast<uint64_t>(extraDataSize);
     }
     MEDIA_INFO_LOG("width:%{public}d, height:%{public}d, size:%{public}" PRId64, photoWidth, photoHeight, size);
+    if (bufferHandle == nullptr) {
+        MEDIA_ERR_LOG("bufferHandle is nullptr");
+        return;
+    }
     int32_t format = bufferHandle->format;
     sptr<CameraPhotoProxy> photoProxy;
     std::string imageIdStr = std::to_string(imageId);
