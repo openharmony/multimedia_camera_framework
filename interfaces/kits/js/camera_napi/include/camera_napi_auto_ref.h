230bfc14bac6067c41826ab3df190c043d51b88a
jango <wangjian422@huawei.com>
2024-08-02 11:08:23 +0800

    AutoRef napi thread safe fix
    Signed-off-by: jango <wangjian422@huawei.com>
    Change-Id: I924b0d4d193afa68657f6c00cc5a48fd8ba8d44c

--- interfaces/kits/js/camera_napi/include/camera_napi_auto_ref.h
+++ interfaces/kits/js/camera_napi/include/camera_napi_auto_ref.h
@@ -23,6 +23,8 @@
 #include "js_native_api.h"
 #include "js_native_api_types.h"
 #include "napi/native_api.h"
+#include "node_api.h"
+#include "node_api_types.h"
 
 namespace OHOS {
 namespace CameraStandard {
@@ -31,6 +33,18 @@ public:
     AutoRef(napi_env env, napi_value callback, bool isOnce) : isOnce_(isOnce), env_(env)
     {
         (void)napi_create_reference(env_, callback, 1, &callbackRef_);
+        napi_value workName;
+        napi_create_string_utf8(env, "~AutoRef", NAPI_AUTO_LENGTH, &workName);
+        napi_create_threadsafe_function(
+            env_, nullptr, nullptr, workName, 0, 1, nullptr, nullptr, nullptr,
+            [](napi_env env, napi_value js_cb, void* context, void* data) {
+                if (data == nullptr) {
+                    return;
+                }
+                napi_ref callbackRef = static_cast<napi_ref>(data);
+                (void)napi_reference_unref(env, callbackRef, nullptr);
+            },
+            &tsfn_);
     }
 
     AutoRef(const AutoRef& other)
@@ -38,6 +52,7 @@ public:
         isOnce_ = other.isOnce_;
         env_ = other.env_;
         callbackRef_ = other.callbackRef_;
+        tsfn_ = other.tsfn_;
         if (env_ != nullptr && callbackRef_ != nullptr) {
             (void)napi_reference_ref(env_, callbackRef_, nullptr);
         }
@@ -49,13 +64,16 @@ public:
         std::swap(isOnce_, tmp.isOnce_);
         std::swap(env_, tmp.env_);
         std::swap(callbackRef_, tmp.callbackRef_);
+        std::swap(tsfn_, tmp.tsfn_);
         return *this;
     }
 
     ~AutoRef()
     {
-        if (env_ != nullptr && callbackRef_ != nullptr) {
-            (void)napi_reference_unref(env_, callbackRef_, nullptr);
+        if (tsfn_ != nullptr && callbackRef_ != nullptr) {
+            if (napi_acquire_threadsafe_function(tsfn_) == napi_ok) {
+                napi_call_threadsafe_function(tsfn_, callbackRef_, napi_tsfn_nonblocking);
+            }
         }
     }
 
@@ -76,6 +94,7 @@ public:
 private:
     napi_env env_ = nullptr;
     napi_ref callbackRef_ = nullptr;
+    napi_threadsafe_function tsfn_ = nullptr;
 };
 } // namespace CameraStandard
 } // namespace OHOS
