e312729d31b562a1a673a3ad7bcea576a5cdfd6f
z30049243 <zhaoyuanchun@huawei.com>
2024-09-03 09:18:41 +0800

    商用分支旋转接口声明
    Signed-off-by: z30049243 <zhaoyuanchun@huawei.com>

--- interfaces/kits/native/include/camera/photo_output.h
+++ interfaces/kits/native/include/camera/photo_output.h
@@ -475,6 +475,20 @@ Camera_ErrorCode OH_PhotoOutput_IsMovingPhotoSupported(Camera_PhotoOutput* photo
  */
 Camera_ErrorCode OH_PhotoOutput_EnableMovingPhoto(Camera_PhotoOutput* photoOutput, bool enabled);
 
+/**
+ * @brief Gets the photo rotation angle.
+ *
+ * @param photoOutput the {@link Camera_PhotoOutput} instance which used to get the photo rotation angle.
+ * @param deviceDegree the current device rotation degree.
+ * @param imageRotation the {@link Camera_ImageRotation} result of photo rotation angle.
+ * @return {@link #CAMERA_OK} if the method call succeeds.
+ *         {@link #CAMERA_INVALID_ARGUMENT} if parameter missing or parameter type incorrect.
+ *         {@link #CAMERA_SERVICE_FATAL_ERROR} if camera service fatal error.
+ * @since 12
+ */
+Camera_ErrorCode OH_PhotoOutput_GetPhotoRotation(Camera_PhotoOutput* photoOutput, int deviceDegree,
+    Camera_ImageRotation* imageRotation);
+
 #ifdef __cplusplus
 }
 #endif
