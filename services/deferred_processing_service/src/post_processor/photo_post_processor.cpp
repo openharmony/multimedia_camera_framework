31fb701726e823e5b17643d717d30a067508d4c7
z30049243 <zhaoyuanchun@huawei.com>
2024-09-12 11:43:23 +0800

    同步代码
    Signed-off-by: z30049243 <zhaoyuanchun@huawei.com>

--- services/deferred_processing_service/src/post_processor/photo_post_processor.cpp
+++ services/deferred_processing_service/src/post_processor/photo_post_processor.cpp
@@ -371,8 +371,9 @@ int32_t PhotoPostProcessor::PhotoProcessListener::OnProcessDoneExt(
         int32_t retFormat = buffer.metadata->Get("deferredImageFormat", deferredImageFormat);
         int32_t retCloudImageEnhanceSupported = buffer.metadata->Get("isCloudImageEnhanceSupported",
             isCloudImageEnhanceSupported);
-        DP_DEBUG_LOG("retImageQuality: %{public}d, retDataSize: %{public}d, retCloudImageEnhanceSupported: %{public}d",
-            static_cast<int>(retImageQuality), static_cast<int>(retDataSize),
+        DP_DEBUG_LOG("retImageQuality: %{public}d, retDataSize: %{public}d, retFormat: %{public}d, "
+            "retCloudImageEnhanceSupported: %{public}d", static_cast<int>(retImageQuality),
+            static_cast<int>(retDataSize),static_cast<int>(retFormat),
             static_cast<int>(retCloudImageEnhanceSupported));
     }
     DP_INFO_LOG("bufferHandle param, bufferHandleSize: %{public}d, dataSize: %{public}d, isDegradedImage: %{public}d, "
