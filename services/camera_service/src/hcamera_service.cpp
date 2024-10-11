bfe7f6f96225f4bc24c8e8009f957c41152cd980
李政 <lizheng194@huawei.com>
2024-10-11 19:02:32 +0800

    补充TAG下发5.0.1
    Signed-off-by: 李政 <lizheng194@huawei.com>

--- services/camera_service/src/hcamera_service.cpp
+++ services/camera_service/src/hcamera_service.cpp
@@ -1734,16 +1734,25 @@ std::shared_ptr<OHOS::Camera::CameraMetadata> HCameraService::CreateDefaultSetti
         int32_t enableValue = item.data.i32[0];
         defaultSettings->addEntry(OHOS_CAMERA_USER_ID, &enableValue, count);
     }
+
     ret = OHOS::Camera::FindCameraMetadataItem(currentSetting->get(), OHOS_CONTROL_MOVING_PHOTO, &item);
     if (ret == CAM_META_SUCCESS) {
         uint8_t enableValue = item.data.u8[0];
         defaultSettings->addEntry(OHOS_CONTROL_MOVING_PHOTO, &enableValue, count);
     }
+    
     ret = OHOS::Camera::FindCameraMetadataItem(currentSetting->get(), OHOS_CONTROL_AUTO_CLOUD_IMAGE_ENHANCE, &item);
     if (ret == CAM_META_SUCCESS) {
         uint8_t filterValue = item.data.u8[0];
         defaultSettings->addEntry(OHOS_CONTROL_AUTO_CLOUD_IMAGE_ENHANCE, &filterValue, count);
     }
+
+    ret = OHOS::Camera::FindCameraMetadataItem(currentSetting->get(), OHOS_CONTROL_BEAUTY_AUTO_VALUE, &item);
+    if (ret == CAM_META_SUCCESS) {
+        uint8_t enableValue = item.data.u8[0];
+        defaultSettings->addEntry(OHOS_CONTROL_BEAUTY_AUTO_VALUE, &enableValue, count);
+    }
+
     uint8_t enableValue = true;
     defaultSettings->addEntry(OHOS_CONTROL_VIDEO_DEBUG_SWITCH, &enableValue, 1);
     return defaultSettings;
