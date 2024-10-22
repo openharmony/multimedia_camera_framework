02dfd196cdd3732110262a9a9ccf44603a3286ce
卢道春 <ludaochun@h-partners.com>
2024-10-22 22:02:01 +0800

    加锁解决mpegManger竞争
    Signed-off-by: 卢道春 <ludaochun@h-partners.com>

--- services/deferred_processing_service/include/post_processor/video_post_processor.h
+++ services/deferred_processing_service/include/post_processor/video_post_processor.h
@@ -84,7 +84,20 @@ private:
         std::lock_guard<std::mutex> lock(sessionMutex_);
         session_ = session;
     }
+    
+    inline sptr<MpegManager> GetMpegManager()
+    {
+        std::lock_guard<std::mutex> lock(mpegManagerMutex_);
+        return mpegManager_;
+    }
+    
+    inline void SetMpegManager(sptr<MpegManager> &mpegManager)
+    {
+        std::lock_guard<std::mutex> lock(mpegManagerMutex_);
+        mpegManager_ = mpegManager;
+    }
 
+    std::mutex mpegManagerMutex_;
     std::mutex sessionMutex_;
     const int32_t userId_;
     int32_t timeoutCount_ {0};
