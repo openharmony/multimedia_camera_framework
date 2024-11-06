a14d606a9630978867cfc9daccebc3be8530b950
wuwenchao <wuwenchao7@huawei.com>
2024-11-06 10:41:41 +0000

    Host崩库时释放隐私回调 （cherry picked commit from <gitee.com//openharmony/multimedia_camera_framework/commit/ab3c40d192e12eeeafdeff73c76367db12013c3b> Signed-off-by: wuwenchao <wuwenchao7@huawei.com>

--- services/camera_service/src/hcamera_device.cpp
+++ services/camera_service/src/hcamera_device.cpp
@@ -1324,6 +1324,7 @@ void HCameraDevice::RemoveResourceWhenHostDied()
     POWERMGR_SYSEVENT_CAMERA_DISCONNECT(cameraID_.c_str());
     NotifyCameraSessionStatus(false);
     NotifyCameraStatus(CAMERA_CLOSE);
+    HandlePrivacyAfterCloseDevice();
     MEDIA_DEBUG_LOG("HCameraDevice::RemoveResourceWhenHostDied end");
 }
 
