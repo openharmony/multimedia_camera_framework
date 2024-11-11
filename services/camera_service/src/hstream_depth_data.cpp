2dabed90fb192d5a208d34887f41e66e702d99f4
z30049243 <zhaoyuanchun@huawei.com>
2024-11-11 14:53:02 +0800

    修复编译问题
    Signed-off-by: z30049243 <zhaoyuanchun@huawei.com>

--- services/camera_service/src/hstream_depth_data.cpp
+++ services/camera_service/src/hstream_depth_data.cpp
@@ -78,8 +78,8 @@ int32_t HStreamDepthData::SetDataAccuracy(int32_t accuracy)
         OHOS::Camera::MetadataUtils::ConvertMetadataToVec(cameraAbility_, ability);
         std::shared_ptr<OHOS::Camera::CameraMetadata> dynamicSetting = nullptr;
         OHOS::Camera::MetadataUtils::ConvertVecToMetadata(ability, dynamicSetting);
-        if (cameraResult == nullptr) {
-            cameraResult = std::make_shared<OHOS::Camera::CameraMetadata>(0, 0);
+        if (dynamicSetting == nullptr) {
+            dynamicSetting = std::make_shared<OHOS::Camera::CameraMetadata>(0, 0);
         }
         camera_metadata_item_t item;
         CHECK_AND_RETURN_RET_LOG(dynamicSetting != nullptr, CAMERA_INVALID_ARG,
