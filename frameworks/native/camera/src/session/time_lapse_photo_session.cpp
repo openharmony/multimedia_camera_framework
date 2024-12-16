8077a8748b07c2716854b85a1a3e891f406a40a8
卢道春 <ludaochun@h-partners.com>
2024-12-16 10:36:35 +0000

    update frameworks/native/camera/src/session/time_lapse_photo_session.cpp.
    Signed-off-by: 卢道春 <ludaochun@h-partners.com>

--- frameworks/native/camera/src/session/time_lapse_photo_session.cpp
+++ frameworks/native/camera/src/session/time_lapse_photo_session.cpp
@@ -51,8 +51,9 @@ std::shared_ptr<OHOS::Camera::CameraMetadata> TimeLapsePhotoSession::GetMetadata
     auto inputDevice = GetInputDevice();
     CHECK_ERROR_RETURN_RET(inputDevice == nullptr, nullptr);
     auto cameraObj = inputDevice->GetCameraDeviceInfo();
-    CHECK_ERROR_RETURN_RET_LOG(!cameraObj,
-                               std::make_shared<OHOS::Camera::CameraMetadata>(DEFAULT_ITEMS, DEFAULT_DATA_LENGTH));
+    if (!cameraObj) {
+        std::make_shared<OHOS::Camera::CameraMetadata>(DEFAULT_ITEMS, DEFAULT_DATA_LENGTH));
+    }                      
     MEDIA_DEBUG_LOG("%{public}s: no physicalCamera, using current camera device:%{public}s", __FUNCTION__,
         cameraObj->GetID().c_str());
     return cameraObj->GetMetadata();
