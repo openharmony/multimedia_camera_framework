66fa19f9a50ebaa42a7b220681ee8540093ce4ba
叶正皓 <yezhenghao@h-partners.com>
2025-03-12 09:10:21 +0800

    setSmoothZoom 抛出103
    Signed-off-by: 叶正皓 <yezhenghao@h-partners.com>

--- frameworks/js/camera_napi/src/session/camera_session_napi.cpp
+++ frameworks/js/camera_napi/src/session/camera_session_napi.cpp
@@ -2414,7 +2414,10 @@ napi_value CameraSessionNapi::SetSmoothZoom(napi_env env, napi_callback_info inf
         int32_t smoothZoomType;
         napi_get_value_double(env, argv[PARAM0], &targetZoomRatio);
         napi_get_value_int32(env, argv[PARAM1], &smoothZoomType);
-        cameraSessionNapi->cameraSession_->SetSmoothZoom((float)targetZoomRatio, smoothZoomType);
+        int32_t retCode = cameraSessionNapi->cameraSession_->SetSmoothZoom((float)targetZoomRatio, smoothZoomType);
+        if (!CameraNapiUtils::CheckError(env, retCode)) {
+            return nullptr;
+        }
     } else {
         MEDIA_ERR_LOG("SetSmoothZoom call Failed!");
     }
