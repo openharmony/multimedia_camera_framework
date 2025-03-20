716c4dd1d92051ad52d8f21dcb0eb9a6bd87476e
zhangchao <zhangchao338@huawei.com>
2025-03-20 11:42:10 +0800

    修复代码告警问题
    Signed-off-by: zhangchao <zhangchao338@huawei.com>

--- services/camera_service/src/hcapture_session.cpp
+++ services/camera_service/src/hcapture_session.cpp
@@ -1411,7 +1411,7 @@ bool StateMachine::Transfer(CaptureSessionState targetState)
 void HCaptureSession::UpdateCameraRotateAngleAndZoom(std::vector<CameraRotateStrategyInfo> &infos,
     std::vector<int32_t> &frameRateRange)
 {
-    int index = -1;
+    size_t index = -1;
     int uid = IPCSkeleton::GetCallingUid();
     std::string bundleName = GetClientBundle(uid);
     for (size_t i = 0; i < infos.size(); i++) {
