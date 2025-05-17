fcb8d86901ff347a36c5a42c48367131cc7a1cac
linchengfeng <linchengfeng1@huawei.com>
2025-05-17 16:34:49 +0800

    enable SetQualityPrioritization interface for sys
    Signed-off-by: linchengfeng <linchengfeng1@huawei.com>

--- frameworks/js/camera_napi/src/mode/video_session_for_sys_napi.cpp
+++ frameworks/js/camera_napi/src/mode/video_session_for_sys_napi.cpp
@@ -47,10 +47,11 @@ napi_value VideoSessionForSysNapi::Init(napi_env env, napi_value exports)
     napi_status status;
     napi_value ctorObj;
 
-    std::vector<std::vector<napi_property_descriptor>> descriptors = { camera_process_props, flash_props,
-        auto_exposure_props, focus_props, zoom_props, filter_props, beauty_props, color_effect_props, macro_props,
-        color_management_props, stabilization_props, preconfig_props, camera_output_capability_props,
-        camera_ability_props, aperture_props, color_reservation_props, effect_suggestion_props};
+    std::vector<std::vector<napi_property_descriptor>> descriptors = {camera_process_props, flash_props,
+        auto_exposure_props, focus_props, zoom_props, filter_props, beauty_props, color_effect_props,
+        quality_prioritization_props, macro_props, color_management_props, stabilization_props, preconfig_props,
+        camera_output_capability_props, camera_ability_props, aperture_props, color_reservation_props,
+        effect_suggestion_props};
     std::vector<napi_property_descriptor> video_session_props = CameraNapiUtils::GetPropertyDescriptor(descriptors);
     status = napi_define_class(env, VIDEO_SESSION_FOR_SYS_NAPI_CLASS_NAME, NAPI_AUTO_LENGTH,
                                VideoSessionForSysNapiConstructor, nullptr,
