8a652fea3bcd7610c2236fb9a57e6400c8701a47
周靖 <zhoujing96@huawei.com>
2025-07-16 09:32:27 +0000

    add ReadyToUnlock mode
    Signed-off-by: 周靖 <zhoujing96@huawei.com>

--- services/camera_service/src/hstream_capture.cpp
+++ services/camera_service/src/hstream_capture.cpp
@@ -571,7 +571,8 @@ bool ConcurrentMap::ReadyToUnlock(const int32_t& key, const int32_t& step, const
     std::lock_guard<std::mutex> lock(map_mutex_);
     if (mode == static_cast<int32_t>(HDI::Camera::V1_3::OperationMode::CAPTURE) ||
       mode == static_cast<int32_t>(HDI::Camera::V1_3::OperationMode::QUICK_SHOT_PHOTO) ||
-      mode == static_cast<int32_t>(HDI::Camera::V1_3::OperationMode::PORTRAIT)) {
+      mode == static_cast<int32_t>(HDI::Camera::V1_3::OperationMode::PORTRAIT) ||
+      mode == static_cast<int32_t>(HDI::Camera::V1_3::OperationMode::CAPTURE_MACRO) {
         return step_.count(key) > 0 && step_[key] == step;
     } else {
         return map_.count(key) > 0;
