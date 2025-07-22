1dbd6f9767cdf2a6f7b0b0bf6fa11d7b126dd1a4
cjj <chengjiajun7@h-partners.com>
2025-07-22 03:20:00 +0000

    update dynamic_libs/moving_photo/src/avcodec/audio_encoder.cpp.
    Signed-off-by: cjj <chengjiajun7@h-partners.com>

--- dynamic_libs/moving_photo/src/avcodec/audio_encoder.cpp
+++ dynamic_libs/moving_photo/src/avcodec/audio_encoder.cpp
@@ -153,9 +153,9 @@ bool AudioEncoder::EnqueueBuffer(sptr<AudioRecord> audioRecord)
         bufferInfo->attr.flags = AVCODEC_BUFFER_FLAGS_NONE;
         CHECK_RETURN_RET_ELOG(!isStarted_, false, "EnqueueBuffer while encoder is not started");
         auto bufferAddr = OH_AVBuffer_GetAddr(bufferInfo->buffer);
+        int32_t bufferCap = OH_AVBuffer_GetCapacity(bufferInfo->buffer);
         CHECK_RETURN_RET_ELOG(bufferCap <= DEFAULT_MAX_INPUT_SIZE, false,
             "Destination buffer capacity is insufficient to hold the data.");
-        int32_t bufferCap = OH_AVBuffer_GetCapacity(bufferInfo->buffer);
         errno_t cpyRet = memcpy_s(bufferAddr, bufferCap, buffer, DEFAULT_MAX_INPUT_SIZE);
         CHECK_RETURN_RET_ELOG(cpyRet != 0, false, "encoder memcpy_s failed. %{public}d", cpyRet);
         int32_t ret = PushInputData(bufferInfo);
