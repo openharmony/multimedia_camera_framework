2616de9e2eb2c0c6a891d6ec071177b8cb7554c6
cjj <chengjiajun7@h-partners.com>
2025-08-25 15:33:47 +0800

    master alarm revise
    Signed-off-by: cjj <chengjiajun7@h-partners.com>

--- interfaces/kits/native/include/camera/capture_session.h
+++ interfaces/kits/native/include/camera/capture_session.h
@@ -995,7 +995,7 @@ Camera_ErrorCode OH_CaptureSession_GetWhiteBalanceMode(Camera_CaptureSession* se
     Camera_WhiteBalanceMode* whiteBalanceMode);
 
 Camera_ErrorCode OH_CaptureSession_GetWhiteBalanceRange(Camera_CaptureSession* session, int32_t *minColorTemperature,
-     int32_t *maxColorTemperature);
+    int32_t *maxColorTemperature);
 
 Camera_ErrorCode OH_CaptureSession_GetWhiteBalance(Camera_CaptureSession* session, int32_t *colorTemperature);
 
