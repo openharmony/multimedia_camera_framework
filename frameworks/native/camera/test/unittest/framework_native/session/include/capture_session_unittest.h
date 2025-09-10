e220811a49c1b667717486ec5013f3bcf001bb5f
guoysh <guoyusheng6@h-partners.com>
2025-09-10 17:05:41 +0800

    补充新增接口的ut用例
    Signed-off-by: guoysh <guoyusheng6@h-partners.com>

--- frameworks/native/camera/test/unittest/framework_native/session/include/capture_session_unittest.h
+++ frameworks/native/camera/test/unittest/framework_native/session/include/capture_session_unittest.h
@@ -134,9 +134,9 @@ class MockCameraSwitchRequestCallback : public OHOS::CameraStandard::CameraSwitc
 
 class MockCameraSwitchCallback : public OHOS::CameraStandard::CameraSwitchCallback {
     void OnCameraActive(const std::string &cameraId, bool isRegisterCameraSwitchCallback, 
-        const CaptureSessionInfo &sessionInfo) override {};
-    void OnCameraUnactive(const std::string &cameraId) override {};
-    void OnCameraSwitch(const std::string &oriCameraId, const std::string &destCameraId, bool status)  override {};
+        const CaptureSessionInfo &sessionInfo) override {}
+    void OnCameraUnactive(const std::string &cameraId) override {}
+    void OnCameraSwitch(const std::string &oriCameraId, const std::string &destCameraId, bool status)  override {}
 };
 }
 }
