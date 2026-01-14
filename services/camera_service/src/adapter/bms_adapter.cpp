25b00b8b71fe8dd3ab7bb7bdc228385a5842efcb
lu-daochun <15323390187@139.com>
2026-01-14 10:01:01 +0800

    update: 更新文件 bms_adapter.cpp 1
    Signed-off-by: lu-daochun <15323390187@139.com>

--- services/camera_service/src/adapter/bms_adapter.cpp
+++ services/camera_service/src/adapter/bms_adapter.cpp
@@ -80,8 +80,7 @@ std::string BmsAdapter::GetBundleName(int uid)
     OHOS::sptrOHOS::ISystemAbilityManager samgr =
         OHOS::SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
     CHECK_RETURN_RET_ELOG(samgr == nullptr, bundleName, "GetClientBundle Get ability manager failed");
-    OHOS::sptrOHOS::IRemoteObject remoteObject =
-        samgr->GetSystemAbility(BUNDLE_MGR_SERVICE_SYS_ABILITY_ID);
+    OHOS::sptrOHOS::IRemoteObject remoteObject = samgr->GetSystemAbility(BUNDLE_MGR_SERVICE_SYS_ABILITY_ID);
     CHECK_RETURN_RET_ELOG(remoteObject == nullptr, bundleName, "GetClientBundle object is NULL.");
     sptrAppExecFwk::IBundleMgr bms = OHOS::iface_castAppExecFwk::IBundleMgr(remoteObject);
     CHECK_RETURN_RET_ELOG(bms == nullptr, bundleName, "GetClientBundle bundle manager service is NULL.");
