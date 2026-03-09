192a8a410489f8d39008f8e2f034cbe2fd6e21e4
m0_59343938 <xuqingyang4@huawei.com>
2026-03-09 19:49:50 +0800

    update: 更新文件 camera_manager_napi.h
    Signed-off-by: m0_59343938 <xuqingyang4@huawei.com>

--- interfaces/kits/js/camera_napi/include/input/camera_manager_napi.h
+++ interfaces/kits/js/camera_napi/include/input/camera_manager_napi.h
@@ -202,8 +202,10 @@ public:
     static napi_value CreateMovieFileOutputInstance(napi_env env, napi_callback_info info);
     static napi_value IsTorchSupported(napi_env env, napi_callback_info info);
     static napi_value IsTorchModeSupported(napi_env env, napi_callback_info info);
+    static napi_value IsTorchLevelControlSupported(napi_env env, napi_callback_info info);
     static napi_value GetTorchMode(napi_env env, napi_callback_info info);
     static napi_value SetTorchMode(napi_env env, napi_callback_info info);
+    static napi_value SetTorchModeOnWithLevel(napi_env env, napi_callback_info info);
     static napi_value On(napi_env env, napi_callback_info info);
     static napi_value Once(napi_env env, napi_callback_info info);
     static napi_value Off(napi_env env, napi_callback_info info);
