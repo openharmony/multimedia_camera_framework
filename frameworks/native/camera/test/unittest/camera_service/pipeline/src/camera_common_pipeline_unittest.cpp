8a8bebbf15395a4e23e5495b9c43b2188e0c0ccf
cjjbf12000 <chengjiajun7@h-partners.com>
2026-06-17 14:29:44 +0800

    fix code
    Signed-off-by: cjjbf12000 <chengjiajun7@h-partners.com>

--- frameworks/native/camera/test/unittest/camera_service/pipeline/src/camera_common_pipeline_unittest.cpp
+++ frameworks/native/camera/test/unittest/camera_service/pipeline/src/camera_common_pipeline_unittest.cpp
@@ -606,14 +606,17 @@ private:
 };
 
 // Test Class Definition
-class UnifiedPipelineUnitTest : public testing::Test {
+class UnifiedPipelineUnitTest : public testing::Test
+{
 protected:
-    void SetUp() override {
+    void SetUp() override
+    {
         // Test initialization
         pipeline_ = std::make_shared<UnifiedPipeline>(4);  // Default 4 threads
     }
 
-    void TearDown() override {
+    void TearDown() override
+    {
         // Test cleanup
         pipeline_.reset();
     }
@@ -777,13 +780,16 @@ HWTEST_F(UnifiedPipelineUnitTest, RemovePlugin_Normal, TestSize.Level0)
     EXPECT_EQ(pipeline_->plugins_.size(), 0);
 }
 
-class UnifiedPipelineManagerTest : public testing::Test {
+class UnifiedPipelineManagerTest : public testing::Test
+{
 protected:
-    void SetUp() override {
+    void SetUp() override
+    {
         // Initialization if needed
     }
     
-    void TearDown() override {
+    void TearDown() override
+    {
         // Cleanup if needed
     }
 };
@@ -1023,13 +1029,16 @@ HWTEST_F(UnifiedPipelineDataProducerTest, OnBufferArrival_NullBuffer, TestSize.L
     producer->OnBufferArrival(nullptr);
 }
 
-class UnifiedPiplinePluginTest : public testing::Test {
+class UnifiedPiplinePluginTest : public testing::Test 
+{
 protected:
-    void SetUp() override {
+    void SetUp() override
+    {
         // Initialization if needed
     }
     
-    void TearDown() override {
+    void TearDown() override
+    {
         // Cleanup if needed
     }
 };
